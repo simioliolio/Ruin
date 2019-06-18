70899d9aa570a2db31f2a5e1086b21d28812d93c
Simon Haycock <shaycock@hotels.com>
2019-06-18 21:46:23 +0100

    Add parameter getters and setters to kernel

--- RuinStutter/RuinStutterKernel.hpp
+++ RuinStutter/RuinStutterKernel.hpp
@@ -10,16 +10,65 @@
 #define RuinStutterKernel_h
 
 #import "RuinAUUtilites/RuinAUUtilites.h"
+#import <iostream>
+#import <algorithm>
 
-// TODO: Declare superclass as public?
+enum {
+    StutterParameterEnable = 0,
+    StutterParameterLength = 1
+};
+
 class RuinStutterKernel : public DSPKernel {
     
 public:
     
     RuinStutterKernel() {}
     
-    void init(int channelCount) {
-        _channelCount = channelCount;
+    void init(int inChannelCount) {
+        channelCount = inChannelCount;
+        enableRamper.init();
+        lengthRamper.init();
+    }
+    
+    void reset() {
+        enableRamper.reset();
+        lengthRamper.reset();
+    }
+    
+    void setParameter(AUParameterAddress address, AUValue value) {
+        switch (address) {
+            case StutterParameterEnable:
+                enableRamper.setUIValue(clamp(value, 0.0f, 1.0f));
+                break;
+            case StutterParameterLength:
+                lengthRamper.setUIValue(clamp(value / 1000.0f, 0.001f, 2.0f));
+                break;
+        }
+    }
+    
+    AUValue getParameter(AUParameterAddress address) {
+        switch (address) {
+            case StutterParameterEnable:
+                return enableRamper.getUIValue();
+                break;
+            case StutterParameterLength:
+                return lengthRamper.getUIValue() * 1000.0f;
+                break;
+            default:
+                std::cout << "Setting unknown parameter" << std::endl;
+                abort();
+        }
+    }
+    
+    void startRamp(AUParameterAddress address, AUValue value, AUAudioFrameCount duration) {
+        switch (address) {
+            case StutterParameterEnable:
+                enableRamper.startRamp(value, duration);
+                break;
+            case StutterParameterLength:
+                lengthRamper.startRamp(clamp(value / 1000.0f, 0.001f, 2.0f), duration);
+                break;
+        }
     }
     
     // TODO: move to superclass?
@@ -32,25 +81,23 @@ public:
         
         for (int frameIndex = 0; frameIndex < frameCount; ++frameIndex) {
             
-            for (int channel = 0; channel < _channelCount; ++channel) {
+            for (int channel = 0; channel < channelCount; ++channel) {
                 outBufferListPtr->mBuffers[channel].mData = inBufferListPtr->mBuffers[channel].mData;
             }
         }
     }
     
-    void startRamp(AUParameterAddress address, AUValue value, AUAudioFrameCount duration) {
-#warning Unimplemented!
-    }
-    
     void handleMIDIEvent(AUMIDIEvent const& midiEvent) {
-#warning Unimplemented!
+        // Does not handle MIDI
     }
     
     
 private:
-    int _channelCount;
+    int channelCount;
     AudioBufferList* inBufferListPtr = nullptr;
     AudioBufferList* outBufferListPtr = nullptr;
+    ParameterRamper enableRamper = 0; // off or on
+    ParameterRamper lengthRamper = 1; // length in s
 };
 
 #endif /* RuinStutterKernel_h */
