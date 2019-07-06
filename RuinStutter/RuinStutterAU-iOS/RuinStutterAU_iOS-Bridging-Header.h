2fef638d57d790efad2e4e547eb7959bdc2ce7e6
Simon Haycock <shaycock@hotels.com>
2019-07-06 15:20:29 +0100

    Remove bridging header

--- RuinStutter/RuinStutterAU-iOS/RuinStutterAU_iOS-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  RuinStutterAU_iOS-Bridging-Header.h
-//  RuinStutterAU-iOS
-//
-//  Created by Simon Haycock on 06/07/2019.
-//  Copyright © 2019 Hyper Barn LTD. All rights reserved.
-//
-
-#import "RuinStutterAudioUnit.h"
