23c3151b2ba9a17c22fe383ef29864c287e6238d
simioliolio <simon@hyperbarn.com>
2019-11-02 11:32:23 +0000

    Add common init and corner radius

--- Ruin/Ruin/Modules/Ruin/XYControl.swift
+++ Ruin/Ruin/Modules/Ruin/XYControl.swift
@@ -10,4 +10,17 @@ import UIKit
 
 @IBDesignable final class XYControl: UIControl {
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        layer.cornerRadius = 5
+    }
 }
