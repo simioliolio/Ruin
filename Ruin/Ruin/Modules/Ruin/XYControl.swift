ea226048c638cf36596bd953f5ddb0bca1d4e78d
simioliolio <simon@hyperbarn.com>
2019-11-02 11:23:02 +0000

    Add XYControl and add to Ruin view

---
+++ Ruin/Ruin/Modules/Ruin/XYControl.swift
@@ -0,0 +1,13 @@
+//
+//  XYControl.swift
+//  Ruin
+//
+//  Created by Simon Haycock on 02/11/2019.
+//  Copyright © 2019 Hyper Barn LTD. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable final class XYControl: UIControl {
+    
+}
