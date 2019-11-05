a4a1477bc7dac89f228e9680f816f26d9759edae
simioliolio <simon@hyperbarn.com>
2019-11-05 18:40:44 +0000

    Add properties to state

--- Ruin/RuinCore/Redux/State.swift
+++ Ruin/RuinCore/Redux/State.swift
@@ -10,4 +10,24 @@ import Foundation
 
 public struct State: ReduxState {
     public var playing: Bool = false
+    public var currentPlaybackPosition: TimeInterval = 0
+    public var audioFileLength: TimeInterval = 0
+    public var positionInteractionState: SliderInteractionState = SliderInteractionState()
+    public var xYInteractionState: (
+        left: XYInteractionState,
+        middle: XYInteractionState,
+        right: XYInteractionState) = (XYInteractionState(),
+                                      XYInteractionState(),
+                                      XYInteractionState())
+        
+}
+
+public struct SliderInteractionState {
+    public var position: CGFloat = 0.0
+    public var enabled: Bool = false
+}
+
+public struct XYInteractionState {
+    public var position: CGPoint = CGPoint(x: 0.5, y: 0.5)
+    public var enabled: Bool = false
 }
